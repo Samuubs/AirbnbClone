3b2877a94d342bb87e72f7a05d4d3f0e43aa3d1e
Samuubs <samuel.santiago170@gmail.com>
2020-01-10 21:18:46 -0300

    Cors + Login

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://samu:samu@cluster0-6iyyt.mongodb.net/airbnb?retr
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
